a6968cafbc3b39ff0d4303c3bd5552a97c4341ce
Diana Kuskildina <diana2x9@yandex.ru>
2023-06-27 23:43:40 +0300

    fixes after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
 const popup = document.querySelector('.popup');
 const editProfileButton = document.querySelector('.profile__edit-button');
-const closeButton = popup.querySelector('.popup__button_close');
+const closeButton = popup.querySelector('.popup__button-close');
 
 const profileName = document.querySelector('.profile__name');
 const profileText = document.querySelector('.profile__text');
-const popupInputName = document.querySelector('.popup__input_name');
-const popupInputWork = document.querySelector('.popup__input_work');
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputWork = document.querySelector('.popup__input_type_work');
 
 const popupForm = document.querySelector('.popup__form');
-const popupButtonSave = document.querySelector('.popup__button_save');
 
 function openPopup() {
   popupInputName.value = profileName.textContent;
@@ -26,9 +25,9 @@ function saveProfile(event) {
   event.preventDefault()
   profileName.textContent = popupInputName.value;
   profileText.textContent = popupInputWork.value;
+  closePopup();
 }
 
 editProfileButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup)
+closeButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', saveProfile);
-popupForm.addEventListener('submit', closePopup);
